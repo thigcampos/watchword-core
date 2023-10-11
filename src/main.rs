aedc17f61dd3d0f31bbf9ae50e5f043f8e077e65
Thiago Campos <thigcampos@proton.me>
2023-10-11 10:41:43 -0300

    minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use rand::Rng;
+
 fn main() {
     let mut i = 0;
-    let mut password: String = "".to_owned();
     const ALPHA: [&str; 26] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    while i < 25 {
+    const NUMBER: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    pub const VALUE: ([&str; 26], [i8; 10]) = (ALPHA, NUMBER);
+    println!("tuple of tuples: {:?}", VALUE);
+    let mut password: String = "".to_owned();
+    while i < 26 {
+        let num = rand::thread_rng().gen_range(0..2);
+
+        println!("{}", VALUE.num);
         password.push_str(ALPHA[i]);
         println!("{}", password);
         i = i + 1;
---
+++ src/values.rs
@@ -0,0 +1,7 @@
+mod constants {
+    const ALPHA: [&str; 26] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const NUMBER: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    pub const VALUE: ([&str; 26], [i8; 10]) = (ALPHA, NUMBER);
+}
+
+
